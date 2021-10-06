98c5a44c418929f33ff3a2294acf679cb0c396b2
Alex <amaklov2002@gmail.com>
2021-10-06 12:55:11 +0400

    Исправления некоторых ошибок  улучшение читаемости кода

--- src/main/java/ru/ssau/tk/tgcvso/practice/tgbot/Bot.java
+++ src/main/java/ru/ssau/tk/tgcvso/practice/tgbot/Bot.java
@@ -10,7 +10,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.security.Key;
 import java.util.Locale;
 
 
--- src/main/java/ru/ssau/tk/tgcvso/practice/tgbot/Main.java
+++ src/main/java/ru/ssau/tk/tgcvso/practice/tgbot/Main.java
@@ -12,7 +12,6 @@ public class Main {
 
         TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
         botsApi.registerBot(new Bot());
-        //System.out.println(GetTopSongs.getTopSongs("kizaru"));
     }
 
 
