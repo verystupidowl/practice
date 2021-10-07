1b23084d801665a48a284e8424bcae06a68c8164
Alex <amaklov2002@gmail.com>
2021-10-07 15:24:36 +0400

    Добавлены библиотеки в pom

--- src/main/java/ru/ssau/tk/tgcvso/practice/tgbot/Main.java
+++ src/main/java/ru/ssau/tk/tgcvso/practice/tgbot/Main.java
@@ -3,16 +3,13 @@ package ru.ssau.tk.tgcvso.practice.tgbot;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 
 public class Main {
     public static void main(String[] args) throws TelegramApiException {
         ApiContextInitializer.init();
 
-        TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+        TelegramBotsApi botsApi = new TelegramBotsApi();
         botsApi.registerBot(new Bot());
     }
-
-
 }
