7156102f9545ce42a519c20b303865512bb4a499
Alex <amaklov2002@gmail.com>
2021-12-09 21:21:46 +0400

    Задание 1.3 из файла

---
+++ src/main/java/ru/ssau/tk/tgcvso/practice/TasksFromFile/Point.java
@@ -0,0 +1,16 @@
+package ru.ssau.tk.tgcvso.practice.TasksFromFile;
+
+public class Point {
+    public final double x;
+    public final double y;
+    public final double z;
+
+    public Point(double x, double y, double z){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+    Point A = new Point(2, 5, 6);
+    Point B = new Point(6.1, 9.12, 82.1);
+    Point C = new Point(543.13, 3456.52, 423.53);
+}
