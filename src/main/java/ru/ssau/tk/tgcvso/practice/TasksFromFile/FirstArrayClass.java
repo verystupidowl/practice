7f503fceaca7fe406a5e30886bd169ece7c18aaa
leha <88672059+plumhaze@users.noreply.github.com>
2021-12-11 19:15:00 +0400

    Задание 2.2 из файла

--- src/main/java/ru/ssau/tk/tgcvso/practice/TasksFromFile/FirstArrayClass.java
+++ src/main/java/ru/ssau/tk/tgcvso/practice/TasksFromFile/FirstArrayClass.java
@@ -4,4 +4,14 @@ public class FirstArrayClass {
     public double[] getArrayBySize(int size){
         return new double[size];
     }
+
+    public int[] getArrayWithOnes(int size){
+        int[] array = new int[size];
+        for(int i = 1; i < array.length - 1; i++){
+            array[i] = 1;
+        }
+        array[0] = 2;
+        array[array.length - 1] = 2;
+        return array;
+    }
 }
--- src/test/java/ru/ssau/tk/tgcvso/practice/TasksFromFile/FirstArrayClassTest.java
+++ src/test/java/ru/ssau/tk/tgcvso/practice/TasksFromFile/FirstArrayClassTest.java
@@ -8,12 +8,17 @@ import java.util.Arrays;
 import static org.testng.Assert.*;
 
 public class FirstArrayClassTest {
-
+    FirstArrayClass object = new FirstArrayClass();
     @Test
-    public void testCreateArrayWithDimension() {
-        FirstArrayClass object = new FirstArrayClass();
+    public void testGetArrayBySize() {
         double[] array = object.getArrayBySize(5);
         double[] knownArray = {0.0, 0.0, 0.0, 0.0, 0.0};
+        Assert.assertTrue(Arrays.equals(array, knownArray));
+    }
+    @Test
+    public void testGetArrayWithOnes() {
+        int[] array = object.getArrayWithOnes(5);
+        int[] knownArray = {2, 1, 1, 1, 2};
         Assert.assertTrue(Arrays.equals(array, knownArray));
     }
 }
