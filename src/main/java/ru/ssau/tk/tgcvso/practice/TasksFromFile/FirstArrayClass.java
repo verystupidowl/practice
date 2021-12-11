3317a309d82a7683aefb73189aecc4bc4e448029
leha <88672059+plumhaze@users.noreply.github.com>
2021-12-11 19:04:59 +0400

    Задание 2.1 из файла

---
+++ src/main/java/ru/ssau/tk/tgcvso/practice/TasksFromFile/FirstArrayClass.java
@@ -0,0 +1,7 @@
+package ru.ssau.tk.tgcvso.practice.TasksFromFile;
+
+public class FirstArrayClass {
+    public double[] getArrayBySize(int size){
+        return new double[size];
+    }
+}
---
+++ src/test/java/ru/ssau/tk/tgcvso/practice/TasksFromFile/FirstArrayClassTest.java
@@ -0,0 +1,19 @@
+package ru.ssau.tk.tgcvso.practice.TasksFromFile;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+
+import static org.testng.Assert.*;
+
+public class FirstArrayClassTest {
+
+    @Test
+    public void testCreateArrayWithDimension() {
+        FirstArrayClass object = new FirstArrayClass();
+        double[] array = object.getArrayBySize(5);
+        double[] knownArray = {0.0, 0.0, 0.0, 0.0, 0.0};
+        Assert.assertTrue(Arrays.equals(array, knownArray));
+    }
+}
